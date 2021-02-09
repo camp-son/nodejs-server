a354817eb23ae844b14c6b0bb48bb123b597e06c
camp-son <sjy2814@gmail.com>
2021-02-09 22:55:57 +0900

    라우팅 메소드, 경로

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Node JS Server exercise
 
-## 초기 설정
+## Initialize
 
 **npm 초기화**
 
@@ -20,6 +20,8 @@ npm i express
 
 ## Routing
 
+### Support method function
+
 - HTTP에서 지원되는 `GET` / `POST` / `PUT` / `DELETE` / `PATCH` 등 다양한 메소드를 지원한다.
 
 ```js
@@ -31,6 +33,77 @@ app.put([PATH], [HANDLER]);
 
 app.delete([PATH], [HANDLER]);
 ```
+
+- `all` 메소드는 메소드와 상관없이 모든 요청에 대해 하나의 경로에서 미들웨어 함수를 로드하는데 사용한다.
+- Handler에서 응답값 설정 또는 next 함수를 호출하지 않으면 요청 상태가 변경되지 않는다.
+
+```js
+app.all([PATH], [HANDLER]);
+```
+
+### Route path
+
+- 라우트 경로는 문자열, 문자열 패턴, 정규식으로 정의할 수 있다.
+- `express`는 `path-to-regexp` 라이브러리를 사용한다.
+- 문자열 일치
+
+  - 지정한 문자열의 경로만 허용한다.
+
+  ```js
+  app.get("/", (req, res) => {
+    res.send("root");
+  });
+
+  app.get("/about", (req, res) => {
+    res.send("about");
+  });
+
+  app.get("/random-text.txt", (req, res) => {
+    res.send("random-text.txt");
+  });
+  ```
+
+- 문자열 패턴
+
+  - `?` `+` `*` `(` `)` 정규식 서브세트이다.
+
+  ```js
+  // /acd or /abcd
+  app.get("/ab?cd", (req, res) => {
+    res.send("ab?cd");
+  });
+
+  // /abcd , /abbcd , /abbbcd etc...
+  app.get("/ab+cd", (req, res) => {
+    res.send("ab+cd");
+  });
+
+  // /abcd , /abycd , /ababcdcd, /abQWERcd etc...
+  app.get("/ab*cd", (req, res) => {
+    res.send("ab*cd");
+  });
+
+  // /abe , /abcde
+  app.get("/ab(cd)?e", (req, res) => {
+    res.send("ab(cd)?e");
+  });
+  ```
+
+- 정규식 패턴
+
+  - 정규식을 기반으로 경로를 지정한다.
+
+  ```js
+  // a 가 포함된 모든 경로
+  app.get(/a/, (req, res) => {
+    res.send("/a/");
+  });
+
+  // a 가 포함된 모든 경로
+  app.get(/\*fly$/, (req, res) => {
+    res.send("/*fly$/");
+  });
+  ```
 
 ## Static resource
 
--- app.js
+++ app.js
@@ -4,20 +4,65 @@ const port = 3000;
 
 app.use('static', express.static(__dirname + 'public'))
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+// app.get('/', (req, res) => {
+//     res.send('Hello World!');
+// });
+
+// app.post('/', (req, res) => {
+//     res.send('Got a POST request');
+// });
+
+// app.put('/user', (req, res) => {
+//     res.send('Got a PUT request at user');
+// });
+
+// app.delete('/user', (req, res) => {
+//     res.send('Got a DELETE request at user');
+// });
+
+// app.all('/secret', (req, res, next) => {
+//     console.log('All path /secret');
+//     next();
+// }, (req, res, next) => {
+//     console.log('Next handler');
+//     // res.send('Secret PATH');
+//     next();
+// });
+
+// app.get("/", (req, res) => {
+//     res.send("root");
+// });
+
+// app.get("/about", (req, res) => {
+//     res.send("about");
+// });
+
+// app.get("/random-text.txt", (req, res) => {
+//     res.send("random-text.txt");
+// });
+
+// app.get("/ab?cd", (req, res) => {
+//     res.send("ab?cd");
+// });
+
+// app.get("/ab+cd", (req, res) => {
+//     res.send("ab+cd");
+// });
+
+// app.get("/ab*cd", (req, res) => {
+//     res.send("ab*cd");
+// });
+
+// app.get("/ab(cd)?e", (req, res) => {
+//     res.send("ab(cd)?e");
+// });
+
+app.get(/a/, (req, res) => {
+    res.send("/a/");
 });
 
-app.post('/', (req, res) => {
-    res.send('Got a POST request');
-});
-
-app.put('/user', (req, res) => {
-    res.send('Got a PUT request at user');
-});
-
-app.delete('/user', (req, res) => {
-    res.send('Got a DELETE request at user');
+app.get(/.*fly$/, (req, res) => {
+    res.send("/.*fly$/");
 });
 
 app.listen(port, () => {
